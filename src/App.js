cb28f45e0ad27c46ea7edad2b3fe8e62e3b0dac6
B Rohan Rao <109647650+rohanlearn@users.noreply.github.com>
2023-05-29 01:35:32 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 import SearchIcon from './search.svg'
 import MovieCard from "./MovieCard";
 import Loader from "./Loader";
-const API_URL = 'http://www.omdbapi.com?apikey=a0978664'
+const API_URL = 'https://www.omdbapi.com?apikey=a0978664'
 const App = ()=>{
     const [movies,setMovies] = useState([])
     const [title,setTitle] = useState("")
