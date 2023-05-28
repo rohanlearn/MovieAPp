c76ad3db9957394ac2dfae9d3edbfd16a84842b6
B Rohan Rao <109647650+rohanlearn@users.noreply.github.com>
2023-05-29 01:24:45 +0530

    first comomit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useEffect, useState } from "react";
+import './App.css'
+import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
+import Loader from "./Loader";
+const API_URL = 'http://www.omdbapi.com?apikey=a0978664'
+const App = ()=>{
+    const [movies,setMovies] = useState([])
+    const [title,setTitle] = useState("")
+    const [loader,setLoader] = useState(false)
+    const searchMovies = async(title)=>{
+        setTimeout(
+            async()=>{
+                const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        if (data.Search) {
+            setLoader(false); // Call the setLoader function
+            setMovies(data.Search); // Set the movies using the data.Search property
+          }
+
+            },700
+        )
+        
+
+    }
+    const searchBtnHandler = ()=>{
+        setLoader(true)
+        searchMovies(title)
+        
+    }
+    const titleHandler = (e)=>{
+        setTitle(e.target.value)
+        
+
+    }
+    useEffect(()=>{
+        
+
+    },[])
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                placeholder="Search for movies"
+                value={title}
+                onChange={titleHandler}
+                />
+                 <img src={SearchIcon}s
+                 alt="search"
+                 onClick={searchBtnHandler}/>
+                 
+            </div>
+                  {
+                    loader && (
+                        <div className="container">
+                            <Loader/>
+                            </div>
+
+                    )
+                  }
+             
+                {(!loader && movies.length>0) && (
+                    <div className="container">
+
+                {movies.map((movie)=>{
+
+                    return(
+                   
+                        <MovieCard movie1={movie}/>
+                     )
+                })}
+                </div>)}
+                
+            
+
+           
+           
+
+        </div>
+        
+        
+    );
 }
 
-export default App;
+
+export default App
